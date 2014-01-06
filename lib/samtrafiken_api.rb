78abd8f52ecda860209c98f06ef2498c22827d8e
Hannes Lilljequist <hannes@sthlmconnection.se>
2014-01-06 22:09:23 +0100

    Cleanup.

--- lib/samtrafiken_api.rb
+++ lib/samtrafiken_api.rb
@@ -5,18 +5,6 @@ require 'samtrafiken_api/resrobot_stops'
 require 'samtrafiken_api/stationinfo'
 require 'json'
 
-# A wrapper for the Samtrafiken web API.
-module SamtrafikenAPI
-  class <<self
-    # Return "Hello world."
-    #
-    # @return [String] the string "Hello World!".
-    def hi
-      return "Hello world!"
-    end
-  end
-end
-
 # Monkey patching HTTParty.
 module HTTParty
   # A custom Parser::json implementation that converts API data to UTF-8.
--- spec/samtrafiken_api_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'samtrafiken_api'
-
-describe SamtrafikenAPI do
-  it "say hi" do
-    SamtrafikenAPI.hi().should eql("Hello world!")
-  end
-end
